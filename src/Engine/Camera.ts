7c3433f5338620e5eba5b532ba6b7981e85b6c00
Igor Kov <igor875126@gmail.com>
2022-12-28 16:48:53 +0600

    Camera shake method added + Helper.sleep method

--- src/Engine/Camera.ts
+++ src/Engine/Camera.ts
@@ -1,11 +1,16 @@
+import Helper from "../Helpers/Helper";
+import Random from "../Helpers/Random";
 import GameObject from "../Objects/GameObject";
 import Core from "./Core";
+import Time from "./Time";
 import Vector2 from "./Vector2";
 
 export default class Camera {
 
     public position: Vector2 = new Vector2(0, 0);
     private core: Core;
+    private shakeSinCounter: number = 0;
+    private shakeOriginalPosition: Vector2 = new Vector2(0, 0);
 
     /**
      * Constructor
@@ -22,5 +27,38 @@ export default class Camera {
             return new Vector2(0, 0);
         }
         return new Vector2(this.position.x - this.core.canvas.width / 2, this.position.y - this.core.canvas.height / 2);
+    }
+
+    /**
+     * Shake camera
+     */
+    public async shake(durationInMs: number, intensity: number): Promise<void> {
+        // Remember original camera position
+        this.shakeOriginalPosition = this.position;
+
+        // Calculate decrease intensity
+        const decreaseIntensityBy = intensity / (durationInMs / 10);
+
+        // Shake camera until we are in time
+        while (durationInMs > 0) {
+            // Calculate x,y shake offset
+            const x = Random.getRandomNumberBetween(-intensity, intensity);
+            const y = Random.getRandomNumberBetween(-intensity, intensity);
+
+            // Change camera position
+            this.position = this.position.add(new Vector2(x, y));
+
+            // Sleep
+            await Helper.sleep(10);
+
+            // Subtract duration, we want that the loop at one point ends
+            durationInMs -= 10;
+
+            // Decrease intensity
+            intensity -= decreaseIntensityBy;
+
+            // Restore camera to it's original position
+            this.position = this.shakeOriginalPosition;
+        }
     }
 }
